7b4bf12ba52128ccf9584e2eee567896597b822f
Obi <obiadrian@gmail.com>
2020-09-17 10:38:39 +0300

    added the antonyms part

--- muse.js
+++ muse.js
@@ -1,15 +1,15 @@
-// reach the API
+// reach the API for rhymes
 
 const url = "https://api.datamuse.com/words?";
 const myParam = "rel_rhy=";
 
-// page elements
+// page elements for rhymes
 
 const inputCuvant = document.querySelector("#cuvant");
 const buton = document.querySelector("#trimite");
 const listare = document.querySelector("#lista");
 
-// render results
+// render results for rhymes
 
 const renderResponse = (res) => {
   if (!res.length) {
@@ -25,7 +25,7 @@ const renderResponse = (res) => {
   return;
 };
 
-// AJAX function
+// AJAX function for rhymes
 const capteazaRezultate = () => {
   const cuvantulScris = inputCuvant.value;
   const urlFinal = `${url}${myParam}${cuvantulScris}`;
@@ -41,7 +41,7 @@ const capteazaRezultate = () => {
   xhr.send();
 };
 
-// display results
+// display rhymes results
 
 const dispAll = (event) => {
   event.preventDefault();
@@ -51,4 +51,57 @@ const dispAll = (event) => {
   capteazaRezultate();
 };
 
+//the button for rhymes
 buton.addEventListener("click", dispAll);
+
+// the antonyms part
+
+//api call
+const urla = "https://api.datamuse.com/words?";
+const param = "rel_ant=";
+
+// my elements
+const inputWord = document.querySelector("#cuvantel");
+const butonas = document.querySelector("#buton");
+
+// AJAX function
+const rezFinal = () => {
+  const wordy = inputWord.value;
+  const urlComplet = `${urla}${param}${wordy}`;
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = "json";
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      renderRawResponse(xhr.response);
+    }
+  };
+  xhr.open("GET", urlComplet);
+  xhr.send();
+};
+
+// display function
+
+const renderRawResponse = (res) => {
+  if (!res.length) {
+    listare.innerHTML = "Please enter a word";
+    return;
+  }
+
+  let words = [];
+  for (let i = 0; i < Math.min(res.length, 10); i++) {
+    words.push(`<li>${res[i].word}</li>`);
+  }
+  words = words.join("");
+
+  listare.innerHTML = `<p>The opposite words are:</p> <ol>${words}</ol>`;
+};
+
+const displayAll = (event) => {
+  event.preventDefault();
+  while (listare.firstChild) {
+    listare.removeChild(listare.firstChild);
+  }
+  rezFinal();
+};
+
+butonas.addEventListener("click", displayAll);
